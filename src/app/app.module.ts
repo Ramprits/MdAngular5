e7fe0d4156681b2e4f3920d0ec6953eaf26f0c84
Ramprits <Rampritsahani@yahoo.com>
2017-11-25 23:45:59 +0530

    ii

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from './typescripts/free';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule, AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MDBBootstrapModule.forRoot(),
+    NgbModule.forRoot(),
+    AgmCoreModule.forRoot({
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en#key
+      apiKey: 'Your_api_key'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
